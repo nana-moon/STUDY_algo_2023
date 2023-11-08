dbf557d6acefcbb99cac5db6ab605f6f04d9a299
nana-moon <logbymoon@naver.com>
2023-11-08 18:58:19 +0900

    [level 2] Title: 후보키, Time: 18.86 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/README.md
+++ 프로그래머스/2/42890. 후보키/README.md
@@ -1,10 +1,10 @@
 # [level 2] 후보키 - 42890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890?language=java) 
 
 ### 성능 요약
 
-메모리: 77.7 MB, 시간: 1.28 ms
+메모리: 75.3 MB, 시간: 18.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 23:57:38
+2023년 11월 3일 18:58:15
 
 ### 문제 설명
 
--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,6 +1,67 @@
+import java.util.*;
 class Solution {
+    int N;
+    int cnt;
+    String[][] relations;
+    int answer = 0;
+    HashSet<TreeSet> candidateKeys = new HashSet<>();
+    
     public int solution(String[][] relation) {
-        int answer = 0;
+        N = relation[0].length;
+        
+        cnt = relation.length;
+        relations = relation;
+        for (int i = 1; i <= N; i++) {
+            combination(0, new TreeSet<>(), i, 0);
+        }
+        
         return answer;
+    }
+    public void combination(int level, TreeSet<Integer> set, int full, int start) {
+        if (level == full) {
+            if (!unique(set)) return;
+            
+            for (TreeSet key : candidateKeys) {
+                if (set.containsAll(key)) return;
+            }
+            candidateKeys.add(set);
+            answer++;
+            return;        
+        }
+        
+        for (int i = start; i < N; i++) {
+            set.add(i);
+            combination(level+1, new TreeSet<>(set), full, i+1);
+            set.remove(i);
+        }
+    }
+    
+    public boolean unique(TreeSet<Integer> set) {
+        HashSet<String> rows = new HashSet<>();
+
+        for (String[] relation : relations) {
+            String newStr = "";
+            for (int key : set) {
+                newStr += relation[key] + ",";
+            }
+            rows.add(newStr);
+        }
+        
+        return rows.size() == cnt;
+        
+        // List<String> list = new ArrayList<>();
+        // // 만들어진 조합으로 중복되는지 검사
+        // for (int i = 0; i < cnt; i++) {
+        //     StringBuilder sb = new StringBuilder();
+        //     for (int idx : set) {
+        //         sb.append(relations[i][idx]);
+        //     }
+        //     if(!list.contains(sb.toString())) {
+        //         list.add(sb.toString());
+        //     } else {
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 }
